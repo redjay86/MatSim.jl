0de9cefc56efcafe90280c760a81c0e8530d76c0
Lance Nelson <lancejnelson@gmail.com>
2025-06-06 16:25:20 -0600

    NS.jl is the script to run NS

--- src/libraries/nSampling.jl
+++ src/libraries/nSampling.jl
@@ -332,11 +332,11 @@ function run_NS(NS::NS,LJ::LennardJones.model)
     cDir = pwd()
 
     i = 1
-#    io = open(joinpath(cDir,"NS.out"),"w")
-#    write(io, "N_walkers = " * string(NS.n_walkers) * "\n")
-#    write(io, "N_cull = " * string(NS.n_cull) * "\n")
-#    write(io, "N_steps_per_walker = " * string(NS.n_iter) * "\n")
-#    write(io, "eps = " * string(NS.eps) * "\n")
+    io = open(joinpath(cDir,"NS.out"),"w")
+    write(io, "N_walkers = " * string(NS.n_walkers) * "\n")
+    write(io, "N_cull = " * string(NS.n_cull) * "\n")
+    write(io, "N_steps_per_walker = " * string(NS.n_iter) * "\n")
+    write(io, "eps = " * string(NS.eps) * "\n")
     perms = zeros(MVector{length(NS.walkers),Int})
 #    while V > NS.eps
     for i= 1:100
@@ -384,12 +384,12 @@ function run_NS(NS::NS,LJ::LennardJones.model)
         end
         i += 1
         V = ((NS.n_walkers - NS.n_cull + 1)/(NS.n_walkers + 1))^i
- #       write(io,string(V) * " ")
- #       write(io,string(E_max) * " \n")
+        write(io,string(V) * " ")
+        write(io,string(E_max) * " \n")
 
     end
 
-#    close(io)
+    close(io)
 end
 
 
